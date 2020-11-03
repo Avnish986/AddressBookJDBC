df1f7c0bdd765c4ebb01051e9b352b0186f05bd0
avnish986 <avnishgupta986@gmail.com>
2020-11-03 19:59:16 +0530

    [Avnish]Added: updating address book by fname and checking in sync with Db

--- src/main/java/com/sql/jdbc/AddressBookDBService.java
+++ src/main/java/com/sql/jdbc/AddressBookDBService.java
@@ -2,22 +2,23 @@ package com.sql.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class AddressBookDBService {
 	private static AddressBookDBService abService;
+	private PreparedStatement preparedStatement;
 	public static AddressBookDBService getInstance() {
-		if(abService == null) {
+		if (abService == null) {
 			abService = new AddressBookDBService();
 		}
 		return abService;
 	}
-	
+
 	private Connection getConnection() throws SQLException {
 		String jdbcURL = "jdbc:mysql://localhost:3306/address_book_service?useSSL=false";
 		String userName = "root";
@@ -26,7 +27,7 @@ public class AddressBookDBService {
 		connection = DriverManager.getConnection(jdbcURL, userName, password);
 		return connection;
 	}
-	
+
 	public List<AddressBookData> readData() {
 		String sql = "select * from address_book;";
 		List<AddressBookData> employeePayrollList = new ArrayList<>();
@@ -39,7 +40,7 @@ public class AddressBookDBService {
 		}
 		return employeePayrollList;
 	}
-	
+
 	private List<AddressBookData> getAddressBookData(ResultSet result) {
 		List<AddressBookData> addressBookList = new ArrayList<>();
 		try {
@@ -53,7 +54,7 @@ public class AddressBookDBService {
 				String zip = result.getString("zip");
 				String phone_no = result.getString("phone_no");
 				String email = result.getString("email");
-				addressBookList.add(new AddressBookData(id, fname, lname, address, city, state, zip,phone_no,email));
+				addressBookList.add(new AddressBookData(id, fname, lname, address, city, state, zip, phone_no, email));
 			}
 			System.out.println(addressBookList);
 		} catch (SQLException e) {
@@ -61,12 +62,20 @@ public class AddressBookDBService {
 		}
 		return addressBookList;
 	}
+	
+	public int updateAddressBookData_Using_PreparedStatement(String fname, String city) {
+		return this.updateAddressBookDataUsingPreparedStatement(fname, city);
+	}
 
-
-
-
-
-
-
+	private int updateAddressBookDataUsingPreparedStatement(String fname, String city) {
+		String sql = String.format("update address_book set city= '%s' where first_name = '%s';", city, fname);
+		try (Connection connection = this.getConnection()) {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			return stmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
 
 }
